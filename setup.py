5d60eb2e8a45befd88907154a33707fedd5148d7
pbannuru <pbannuru@gmail.com>
2022-07-17 13:19:06 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup,find_packages
+from typing import List
+
+#Declaring variables for setup functions
+PROJECT_NAME="flight_fare-predictor"
+VERSION="0.0.1"
+AUTHOR="pavan kumar"
+DESRCIPTION="This is a Machine Learning End to End Project"
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list()->List[str]:
+    """
+   
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove("-e .")
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), # return all the folder names where__init__.py is avilable and then install
+install_requires=get_requirements_list()
+)
+
+# __init__.py means library is made that can be import
